67ebc4637b44c12b08525d918ce5b5ee0ec00857
Do jin <kimdojin3380@gmail.com>
2025-12-05 13:40:12 +0900

    Delete model directory

--- model/__init__.py
+++
@@ -1,7 +0,0 @@
-# model/__init__.py
-from .model_drone import DroneMultiClassifier
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "leaf_pytorch"))
-
-__all__ = ["DroneMultiClassifier"]
